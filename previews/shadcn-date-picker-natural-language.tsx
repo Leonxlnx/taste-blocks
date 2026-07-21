723ff50c0af28d7125ef03e4ea669447003f79b7
Leonxlnx <lexn.lin8@gmail.com>
2026-07-22 01:03:50 +0200

    Import Taste Blocks component library

---
+++ previews/shadcn-date-picker-natural-language.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import { DatePickerNaturalLanguage } from "@/registry/sources/shadcn-ui/components/shadcn-date-picker-natural-language/date-picker-natural-language"
+
+export default function ShadcnDatePickerNaturalLanguagePreview() {
+  return <DatePickerNaturalLanguage name="publish-date" />
+}
